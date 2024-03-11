af0b846f8a9cf621eec718f670aa9876d74ec115
SamTov <tovey.samuel@gmail.com>
2024-03-11 08:33:00 +0100

    Update vis

--- microrobotics-lab/temperature-and-strategy/rod-rotation/vis.py
+++ microrobotics-lab/temperature-and-strategy/rod-rotation/vis.py
@@ -6,17 +6,17 @@ import sys
 
 prefix=sys.argv[1]
 
-with hf.File(f"{prefix}/trajectory.hdf5") as db:
-    agents = db["colloids"]["Unwrapped_Positions"][:, :50, :][:] - np.array([500., 500., 0.])
-    rod = db["colloids"]["Unwrapped_Positions"][:, 50:, :][:] - np.array([500., 500., 0.])
+with hf.File(f"{prefix}") as db:
+    agents = db["colloids"]["Unwrapped_Positions"][:, :20, :][:] - np.array([150., 150., 0.])
+    rod = db["colloids"]["Unwrapped_Positions"][:, 20:, :][:] - np.array([150., 150., 0.])
 
 colloid_material = vis.Material(colour=np.array([30, 144, 255]) / 255)
-colloid_mesh = vis.Sphere(radius=2.14, material=colloid_material, resolution=5)
+colloid_mesh = vis.Sphere(radius=2.14, material=colloid_material, resolution=2)
 colloid_particle = vis.Particle(name="Colloid", mesh=colloid_mesh, position=agents)
 
 # Create rod colloid mesh
 rod_material = vis.Material(colour=np.array([255, 140, 0]) / 255)
-rod_mesh = vis.Sphere(radius=2.14, material=rod_material, resolution=5)
+rod_mesh = vis.Sphere(radius=2.14, material=rod_material, resolution=2)
 rod_particle = vis.Particle(name="Rod", mesh=rod_mesh, position=rod)
 
 # Run the visualizer
